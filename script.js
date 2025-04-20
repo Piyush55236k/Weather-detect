2cccc7d40df34a04393e46508e15292217f8e7fe
Piyush55236k <131640134+Piyush55236k@users.noreply.github.com>
2025-04-21 01:20:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
 const api_key = `1abaef6b2e5f38e5e84efb3089360aa8`;
 const api_key2=`f8ed52d0278c91608390b532e30e7b63`;
 let dataDiv;
@@ -18,7 +19,6 @@ const getData = async () => {
     try {
         const response = await fetch(newUrl);
         const data = await response.json();
-        console.log("API Response:", data);
 
         if (!data.current || !data.location) {
             alert("Invalid city or no data found.");
@@ -35,8 +35,8 @@ const getData = async () => {
             <p>The temperature in ${city} is ${data.current.temperature}°C</p>
             <p>The Wind Direction is: ${data.current.wind_dir}</p>
             <p>The Wind Speed is: ${data.current.wind_speed}</p>
-            <p>The Latitude is: ${data.location.latitude}</p>
-            <p>The Longitude is: ${data.location.longitude}</p>
+            <p>The Latitude is: ${data.location.lat}</p>
+            <p>The Longitude is: ${data.location.lon}</p>
             <p>The Local Time is: ${data.location.localtime}</p>
         `;
     } catch (error) {
